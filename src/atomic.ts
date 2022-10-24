4a3f0fabbc5d9f0b4691e9ea5460896ad31b915e
curious33 <curious.startpro@gmail.com>
2022-10-24 15:14:39 +0500

    feat!: automatic export import components add several important features: 1. Automatic export import components 2. Override default configs with new configs 3. Add actions according to the given data(dynamic actions)

--- src/atomic.ts
+++ src/atomic.ts
@@ -1,10 +1,42 @@
-import { NodePlopAPI } from 'node-plop'
+import { NodePlopAPI, Actions, Prompts, DynamicActionsFunction, ActionType } from 'node-plop'
 import path from 'path'
+import fs from 'fs'
 import { GeneratorConfig } from './index'
 
-const atomicComponent = (config: Partial<GeneratorConfig>, plop: NodePlopAPI) => {
-  const prompts = []
-  const actions = []
+const atomicComponent = (plop: NodePlopAPI, config: Partial<GeneratorConfig>) => {
+  const prompts: Prompts = []
+
+  const defaultConfig: GeneratorConfig = {
+    createIndex: true,
+    functional: true,
+    basePath: 'src/components',
+    test: true,
+    story: true,
+    style: true,
+  }
+
+  const fullConfig: GeneratorConfig = {
+    ...defaultConfig,
+    ...config,
+  }
+
+  prompts.push({
+    type: 'input',
+    name: 'name',
+    message: 'component name: ',
+  })
+
+  prompts.push({
+    type: 'input',
+    name: 'type',
+    message: 'DOM API type: ',
+  })
+
+  prompts.push({
+    type: 'input',
+    name: 'tag',
+    message: 'tag name: ',
+  })
 
   // prompts.push({
   //   type: 'list',
@@ -13,19 +45,49 @@ const atomicComponent = (config: Partial<GeneratorConfig>, plop: NodePlopAPI) =>
   //   choices: ['Atom', 'Molecule', 'Organism', 'Template', 'Page'],
   // })
 
-  const CURRENT_DIR = path.resolve(__dirname)
+  const actions: DynamicActionsFunction = (
+    data: { [key: string]: string } | undefined,
+  ): ActionType[] => {
+    const CURRENT_DIR = path.resolve(__dirname)
+    const actions: Actions = []
 
-  prompts.push({
-    type: 'input',
-    name: 'name',
-    message: 'component name',
-  })
+    console.log({ data })
 
-  actions.push({
-    type: 'add',
-    path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
-    templateFile: CURRENT_DIR + '/templates/test.ts.hbs',
-  })
+    if (fullConfig.createIndex) {
+      const indexPath = fullConfig.basePath + '/index.ts'
+      if (!fs.existsSync(fullConfig.basePath)) {
+        fs.mkdirSync(fullConfig.basePath)
+        fs.writeFileSync(indexPath, `// Component imports \n\nexport {\n// Component exports\n}`, {
+          encoding: 'utf8',
+        })
+      }
+
+      actions.push({
+        type: 'modify',
+        path: indexPath,
+        template: "import { {{pascalCase name}} } from './{{pascalCase name}}';\n$1",
+        pattern: /(\/\/ Component imports)/g,
+      })
+      actions.push({
+        type: 'modify',
+        path: indexPath,
+        template: '\t{{pascalCase name}},\n$1',
+        pattern: /(\/\/ Component exports)/g,
+      })
+    }
+
+    // console.log({ config, plop })
+
+    actions.push({
+      type: 'addMany',
+      destination: fullConfig.basePath + '/{{pascalCase name}}',
+      templateFiles: CURRENT_DIR + '/templates/ts/components/*.hbs',
+      base: CURRENT_DIR + '/templates/ts/components',
+      // skipIfExists: true,
+    })
+
+    return actions
+  }
 
   return {
     description: 'react component',
